b254653e55ae38045c29c8b3f554a1e9da8cb2e8
Devesh Shukla <deveshshukla1603@gmail.com>
2024-01-01 21:08:10 +0530

    Cors

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
-/**
-  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
-  - Use built in Node.js `fs` module
-  The expected API endpoints are defined below,
-  1. GET /files - Returns a list of files present in `./files/` directory
-    Response: 200 OK with an array of file names in JSON format.
-    Example: GET http://localhost:3000/files
-  2. GET /file/:filename - Returns content of given file by name
-     Description: Use the filename from the request path parameter to read the file from `./files/` directory
-     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
-     Example: GET http://localhost:3000/file/example.txt
-    - For any other route not defined in the server return 404
-    Testing the server - run `npm run test-fileServer` command in terminal
- */
-    const express = require('express');
-    const fs = require('fs');
-    const path = require('path');
-    const app = express();
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors());
+
+app.get("/files",(req,res)=>{
+  fs.readdir("./Files",(err,files)=>{
+    if(err)
+    {
+      console.error("Error :",err);
+      return;
+    }
+    res.json(200,files);
+  })
+});
+
+app.get("/files/:filename",(req,res)=>{
+  const fileName = req.params.filename;
+  fs.readFile("./Files/"+fileName,(err,val)=>{
+    if(err)
+    {
+      console.error("Error ",err)
+      res.send(404);
+      return;
+    }
+    res.json(val.toString());
     
-    app.get("/files",(req,res)=>{
-      fs.readdir("./Files",(err,files)=>{
-        if(err)
-        {
-          console.error("Error :",err);
-          return;
-        }
-        res.json(200,files);
-      })
-    });
-    
-    app.get("/files/:filename",(req,res)=>{
-      const fileName = req.params.filename;
-      fs.readFile("./Files/"+fileName,(err,val)=>{
-        if(err)
-        {
-          console.error("Error ",err)
-          res.send(404);
-          return;
-        }
-        res.json(val.toString());
-        
-      })
-    });
-    
-    app.listen(3000,()=>{console.log("STarted");});
-    
-    
-    module.exports = app;
+  })
+});
+
+app.listen(3000,()=>{console.log("STarted");});
+
+
+module.exports = app;
